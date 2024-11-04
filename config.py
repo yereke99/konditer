ba5e22368132a35ed05a8d41e0e8afa13287ae9f
yereke99 <yerek.yerkinbekuly@contractor.calleridreputation.com>
2024-11-04 16:25:10 +0500

    main: added some code

---
+++ config.py
@@ -0,0 +1,10 @@
+api_token = '7636447344:AAHWautzupWN26gxjqw7BYnnL9O_x7NVFm8'
+
+api_token = '7554839219:AAEuNFkVEvzEucOaDW-oZs3Sq1X6d-DerVo'
+
+admin = 800703982
+
+admin2 = 6768397766
+
+admin3 = 685953723
+
